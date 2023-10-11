b39e97c8eedbb79825602beefa6d7e7fec4c2051
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2023-10-11 13:35:40 +0000

    Deploy VNET

---
+++ solutions/security-lab/virtual_network.tf
@@ -0,0 +1,34 @@
+#
+# Resource Group
+#
+
+resource "azurecaf_name" "rg_vnet" {
+  name          = "vnet"
+  resource_type = "azurerm_resource_group"
+  prefixes      = []
+  suffixes      = [local.project_shortname, "01"]
+  clean_input   = true
+}
+
+resource "azurerm_resource_group" "rg_vnet" {
+  name     = azurecaf_name.rg_vnet.result
+  location = local.config.generic.regions.primaryRegion.name
+  tags     = local.tags
+}
+
+
+#
+# Network
+#
+
+# This VNET setup is purely for demo purposes. In production, make sure to have separate VNETs and
+# use the VNET peering functionality to bring them together with a firewall in the middle.
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.rg_vnet.name
+  subnet_prefixes     = ["172.16.20.0/24", "172.16.21.0/24"]
+  subnet_names        = ["tier-0", "tier-1"]
+
+  depends_on = [azurerm_resource_group.rg_vnet]
+}
