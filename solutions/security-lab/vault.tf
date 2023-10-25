bae93c2f55a877561dd6d40288850246719e80b4
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2023-10-25 15:55:02 +0000

    Fix app id

--- solutions/security-lab/vault.tf
+++ solutions/security-lab/vault.tf
@@ -45,8 +45,8 @@ resource "azurerm_key_vault" "generic_kv" {
   }
 
   access_policy {
-    tenant_id = local.config.generic.org.tenant_id
-    object_id = "16387a48-4460-410a-a7eb-07040ede67b2" # App ID of Terraform account used to deploy this lab
+    tenant_id      = local.config.generic.org.tenant_id
+    application_id = "16387a48-4460-410a-a7eb-07040ede67b2" # App ID of Terraform account used to deploy this lab
     secret_permissions = [
       "Get",
       "Set",
