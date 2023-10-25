69dad36ee9d96fee750f987cc4bf046cf9e82941
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2023-10-25 16:11:12 +0000

    Update KV firewall

--- solutions/security-lab/vault.tf
+++ solutions/security-lab/vault.tf
@@ -39,9 +39,9 @@ resource "azurerm_key_vault" "generic_kv" {
   sku_name = "standard"
 
   network_acls {
-    default_action = "Deny"
+    default_action = "Allow"
     bypass         = "AzureServices"
-    ip_rules       = []
+    # ip_rules       = []
   }
 
   access_policy {
