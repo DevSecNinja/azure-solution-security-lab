02ced3f4543ebeddc16e18834f853eb9ee37722f
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2023-11-28 20:17:17 +0000

    Add address spaces

--- solutions/security-lab/vm_evilginx.tf
+++ solutions/security-lab/vm_evilginx.tf
@@ -106,6 +106,7 @@ module "evilginx_network" {
   use_for_each = true
 
   resource_group_name = azurerm_resource_group.rg_vm_evilginx.name
+  address_spaces      = ["192.168.250/24"]
   subnet_prefixes     = ["192.168.250/24"]
   subnet_names        = ["primary"]
 
