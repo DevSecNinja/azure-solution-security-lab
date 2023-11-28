71cea2efc73e209809d804921b76de96d53cd62e
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2023-11-28 20:00:03 +0000

    Comment SSH key enablement

--- solutions/security-lab/vm_evilginx.tf
+++ solutions/security-lab/vm_evilginx.tf
@@ -57,8 +57,8 @@ module "evilginx_vms" {
   vnet_subnet_id         = module.evilginx_network.vnet_subnets[0]
   name_template_vm_linux = "$${vm_hostname}-vml-$${host_number}"
 
-  enable_ssh_key = true
-  ssh_key_values = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDrGDCRDUvdnPFL9TGgICVWz9+qV21IYDpwf9P5tg73n5H0ZhA5Tn9ogo9VdzSrDMaXZ2/Ca1PQDEtjb7Q/6NDC9XQbL0DOsi0wyQiTcJopdZnyH6jYVwWUhJ1hXeEsmD9eVTfXwCelPzJpJU2um/w0UR5EhX6nI1Mh7koLjEycmXQLTZtkZlnJttdI63ka7xeDZbQSwbpaGpZkFlFTahjc7OTw1416jL9Ld3Mu2VF0Sg+MpJS4jUiJB7DaSI2RTQZSnMXTQe/uPy9Fj8npg8ti0kjnM4jf9hYYudq8nznnfSvC8tY44RL42Kh+K/H79tyfMTwPIOuww1hytSkJg7EAMv/o7zAZYyiQzOWIxWqlMFr1+8/laIkVnm9bP9tLN4mHdq4SBQA7woj6VWSKrplN13sOIDFXit726SMxEPFma87c1vUYdrvcZvDz7dSeYVvlz8Fv/pwuz6/KMShCVAtutvyHAdOfEwUHX/x/Bh9Yp6dnn2BdgNHyCnSbFc/J4fy7SMbWoi3DViTxsx/xycVW/EuoAehWmyWUO0+p5RYgauEmnBSDqAt6tIGy25+7p7ZPfZRSzG+02vNt+lWgcUcMehmfxy+ROOqNwvslrsKWp3r3cYszMMDhzrgVrxmiNeHO+tlrZGvO6euv1kNLCxCbQy7A7WCyyry4fBT2UBVYOw=="] # TOOD: To replace hardcoded key with GitHub Data Source
+  # enable_ssh_key = true # TODO: Raise bug on compute module for failing SSH keys in GH Actions
+  # ssh_key_values = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDrGDCRDUvdnPFL9TGgICVWz9+qV21IYDpwf9P5tg73n5H0ZhA5Tn9ogo9VdzSrDMaXZ2/Ca1PQDEtjb7Q/6NDC9XQbL0DOsi0wyQiTcJopdZnyH6jYVwWUhJ1hXeEsmD9eVTfXwCelPzJpJU2um/w0UR5EhX6nI1Mh7koLjEycmXQLTZtkZlnJttdI63ka7xeDZbQSwbpaGpZkFlFTahjc7OTw1416jL9Ld3Mu2VF0Sg+MpJS4jUiJB7DaSI2RTQZSnMXTQe/uPy9Fj8npg8ti0kjnM4jf9hYYudq8nznnfSvC8tY44RL42Kh+K/H79tyfMTwPIOuww1hytSkJg7EAMv/o7zAZYyiQzOWIxWqlMFr1+8/laIkVnm9bP9tLN4mHdq4SBQA7woj6VWSKrplN13sOIDFXit726SMxEPFma87c1vUYdrvcZvDz7dSeYVvlz8Fv/pwuz6/KMShCVAtutvyHAdOfEwUHX/x/Bh9Yp6dnn2BdgNHyCnSbFc/J4fy7SMbWoi3DViTxsx/xycVW/EuoAehWmyWUO0+p5RYgauEmnBSDqAt6tIGy25+7p7ZPfZRSzG+02vNt+lWgcUcMehmfxy+ROOqNwvslrsKWp3r3cYszMMDhzrgVrxmiNeHO+tlrZGvO6euv1kNLCxCbQy7A7WCyyry4fBT2UBVYOw=="] # TOOD: To replace hardcoded key with GitHub Data Source
 
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
